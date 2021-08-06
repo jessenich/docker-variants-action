5026245484d8177814dfe91cc987b352caa572ba
Jesse N <jesse@keplerdev.com>
2021-08-06 15:03:24 -0400

    updated test action

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-act "on: push" --actor "jessenich/docker-variants-action" --secret-file "./.secrets" --pull -W "./../.github/workflows/test-action"
+act "on: push" --actor "jessenich/docker-variants-action" --secret-file "tests/.secrets" --pull
